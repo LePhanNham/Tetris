9f4d423e680d8d120c4f8e9266f833d89ffa722d
LePhanNham <lenham2k3@gmail.com>
2025-10-01 19:51:13 +0700

    Setup asset,create data

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+
+public enum Tetromino
+{
+    I, O, T, S, Z, J, L
+}
+[System.Serializable]
+public struct TetrominoData
+{
+    public Tetromino tetromino;
+    public Tile tile;
+    public Vector2Int[] cells; 
+}
