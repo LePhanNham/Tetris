f7ceff613ff938b951738aa091aa74d39f32088c
LePhanNham <lenham2k3@gmail.com>
2025-10-06 17:02:21 +0700

    updated control pieces

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -13,17 +13,18 @@ public class TetrominoData
     public Tetromino tetromino;
     public Tile tile;
     public Vector2Int[] cells; 
-
+    public Vector2Int[,] wallKicks { get;private set; }
     public void Initialize()
     {
         this.cells = Data.Cells[this.tetromino];
+        this.wallKicks = Data.WallKicks[this.tetromino];
     }
 
     public void GetCells()
     {
         foreach (var cell in this.cells)
         {
-            Debug.Log(cell);    
+            // Debug.Log(cell);    
         }
     }
 }
